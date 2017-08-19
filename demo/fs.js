5015f4a9d7243b27a0f1df23b7051ae006c0b4ef
weiqiang165260 <mushangwq@163.com>
2017-08-19 19:10:46 +0800

    使用 fs 模块。

---
+++ demo/fs.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+var server =http.createServer(function (req,res) {
+    var pathname = url.parse(req.url).pathname;
+    var filepath = path.join("/tmp",'wwwroot',pathname);
+    var stream = fs.createReadStream(filepath,{
+        flags:'r',
+        encoding:null
+    });
+    stream.on('error',function () {
+        res.writeHead(404);
+        res.end();
+    })
+    stream.pipe(res);
+})
+server.on("error",function (error) {
+    console.log("error:"+error);
+})
+server.listen("8080","127.0.0.1");
